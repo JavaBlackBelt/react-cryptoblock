535c803e3db82a00089969e35d76be5f20343272
Brajendra <10771397+JavaBlackBelt@users.noreply.github.com>
2019-01-08 16:05:10 +0800

    Update json Server Port and start script

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
     };
   }
   componentDidMount() {
-    fetch("http://localhost:3004/contracts/")
+    fetch("http://" + window.location.hostname + ":8086/contracts/")
       .then(res => res.json())
       .then(json => {
         this.setState({ isloaded: true, contracts: json });
@@ -41,86 +41,83 @@ class App extends Component {
 
   render() {
     var { isloaded, contracts } = this.state;
-    if (!isloaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-          </header>
-          <table class="rwd-table">
-            <tr>
-              <td data-th="Market Analysis" position="relative" height="100%">
-                <LiveChart />
-              </td>
-              <td data-th="Latest Contract" position="relative" height="100%">
-                <contractStyle className="App-contract">
-                  <p>
-                    Latest Contracts
-                    <button
-                      type="button"
-                      class="btn btn-success"
-                      data-toggle="modal"
-                      data-target="#contractModal"
-                    >
-                      Create Contract
-                    </button>
-                  </p>
-                </contractStyle>
-                <Table striped responsive>
-                  <rowStyle className="App-row">
-                    <thead>
-                      <tr>
-                        <th>ID</th>
-                        <th>Owner</th>
-                        <th>Amount</th>
-                        <th>Corrency</th>
-                        <th>Date</th>
-                      </tr>
-                    </thead>
-                  </rowStyle>
-                  <colStyle className="App-col">
-                    <tbody responsive>
-                      <ul>
-                        {contracts.map(contract => (
-                          <li key={contract.id}>
-                            <tr>
-                              <td>{contract.id}</td>
-                              <td>
-                                {contract.user.name} {contract.user.surname}
-                              </td>
-                              <td>{contract.amountInUsd}</td>
-                              <td>{contract.currency}</td>
-                              <td>{contract.date}</td>
-                              <Button
-                                bsStyle="danger"
-                                bsSize="xsmall"
-                                data-toggle="model"
-                                data-target="#"
-                                onClick={this.deleteContract.bind(
-                                  this,
-                                  contract.id
-                                )}
-                              >
-                                Delete
-                              </Button>
-                            </tr>
-                          </li>
-                        ))}
-                      </ul>
-                    </tbody>
-                  </colStyle>
-                </Table>
-              </td>
-            </tr>
-          </table>
-          <footer className="App-footer">
-            <p>@Copyright Crypto Blocks 2018</p>
-          </footer>
-        </div>
-      );
-    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <table class="rwd-table">
+          <tr>
+            <td data-th="Market Analysis" position="relative" height="100%">
+              <LiveChart />
+            </td>
+            <td data-th="Latest Contract" position="relative" height="100%">
+              <contractStyle className="App-contract">
+                <p>
+                  Latest Contracts
+                  <button
+                    type="button"
+                    class="btn btn-success"
+                    data-toggle="modal"
+                    data-target="#contractModal"
+                  >
+                    Create Contract
+                  </button>
+                </p>
+              </contractStyle>
+              <Table striped responsive>
+                <rowStyle className="App-row">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Owner</th>
+                      <th>Amount</th>
+                      <th>Corrency</th>
+                      <th>Date</th>
+                    </tr>
+                  </thead>
+                </rowStyle>
+                <colStyle className="App-col">
+                  <tbody responsive>
+                    <ul>
+                      {contracts.map(contract => (
+                        <li key={contract.id}>
+                          <tr>
+                            <td>{contract.id}</td>
+                            <td>
+                              {contract.user.name} {contract.user.surname}
+                            </td>
+                            <td>{contract.amountInUsd}</td>
+                            <td>{contract.currency}</td>
+                            <td>{contract.date}</td>
+                            <Button
+                              bsStyle="danger"
+                              bsSize="xsmall"
+                              data-toggle="model"
+                              data-target="#"
+                              onClick={this.deleteContract.bind(
+                                this,
+                                contract.id
+                              )}
+                            >
+                              Delete
+                            </Button>
+                          </tr>
+                        </li>
+                      ))}
+                    </ul>
+                  </tbody>
+                </colStyle>
+              </Table>
+            </td>
+          </tr>
+        </table>
+        <footer className="App-footer">
+          <p>@Copyright Crypto Blocks 2019</p>
+        </footer>
+      </div>
+    );
   }
 }
+
 export default App;
